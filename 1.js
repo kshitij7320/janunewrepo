66ef3ae5926857fed213d49f58103312c4673f17
[kshitij himanshu] <[kshitij7320@gmail.com]>
2025-03-11 19:48:47 +0530

    update

---
+++ 1.js
@@ -0,0 +1,19 @@
+// console.log("hello");
+// fullname = "John Doe";
+// age = 24;
+// price = 99.99;
+// x = null;
+// y = undefined;
+// console.log(x);
+// let name = prompt("hello");
+// console.log(name);
+// let num = prompt("enter the number")
+// if(num % 5 === 0){
+//     console.log("num is divisible of 5");
+// }
+// else{
+//     console.log("num is not divisible of 5");
+// }
+for (let i = 1; i <= 100000; i++) {
+    console.log("apna college");
+}
