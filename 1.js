adaae07d755c5eed753bd6f565716b15ac470d9d
[kshitij himanshu] <[kshitij7320@gmail.com]>
2025-03-15 13:35:09 +0530

    update

--- 1.js
+++ 1.js
@@ -14,6 +14,25 @@
 // else{
 //     console.log("num is not divisible of 5");
 // }
-for (let i = 1; i <= 100000; i++) {
-    console.log("apna college");
+// for (let i = 1; i <= 100000; i++) {
+//     console.log("apna college");
+// }
+// let items = [250 , 250 , 250 , 250 , 102]
+
+// let idx = 0;
+// for (let i = 0; i < items.length; i++) {
+//     if (items[i] > 200) {
+//         idx = i;
+//         break;
+//     }
+// }
+// for (let val of items) {
+//     console.log("value at index &{idx) = ${val)");
+//     idx++;
+// }
+function kshitij()
+{
+    console.log("welcome kshitij");
+    console.log("hello kshitij");
 }
+kshitij();
